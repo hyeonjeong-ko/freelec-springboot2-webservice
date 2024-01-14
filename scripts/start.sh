940369f33f789c9fe7885a53dad52d19a76df0cb
hyeonjeong ko <72601276+hyeonjeong-ko@users.noreply.github.com>
2024-01-14 22:27:56 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,7 +6,7 @@ source ${ABSDIR}/profile.sh
 
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=springboot-web-practise
+PROJECT_NAME=freelec-springboot3-webservice
 
 echo "> Build 파일 복사"
 echo "cp $REPOSITORY/zip/*.jar $REPOSITORY/"
@@ -15,7 +15,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/springboot-webservice-1.0.1-SNAPSHOT.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
@@ -29,6 +29,6 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다. "
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties\
+  -Dspring.config.location=classpath:/application.yml,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties\
   -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+  /home/ec2-user/app/step2/springboot-webservice-1.0.1-SNAPSHOT.jar > $REPOSITORY/nohup.out 2>&1 &
